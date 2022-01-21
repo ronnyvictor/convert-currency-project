c5f28c673bcc454184de130ef6b0f1980f8461d2
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-01-21 14:41:21 -0500

    changed spacing

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ console.log(`${USD} USD is equivalent to ${convertUSDtoCAD(USD)} CAD`)
 
 const tips = [5, 8, 13, 2, 14, 27, 9, 18]
 
-
 function calcTips (arr){
     let sum = 0
     for (let i=0; i < arr.length; i++ ){
