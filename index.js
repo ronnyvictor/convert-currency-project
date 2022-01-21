a26ff6787b76d79fcb005acc9ec7efdc759bb6cb
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-01-21 13:36:39 -0500

    added function

---
+++ index.js
@@ -0,0 +1,9 @@
+const USD = 95
+
+function convertUSDtoCAD (amountUSD){
+     return amountUSD * 1.26
+}
+
+// console.log(convertUSDtoCAD(USD))
+
+console.log(`${USD} is equivalent to ${convertUSDtoCAD(USD)}`)
