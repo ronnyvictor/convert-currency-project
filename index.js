a1537f318b2094f9e1b258a4128c223e1001697c
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-01-21 14:30:11 -0500

    added new function

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function convertUSDtoCAD (amountUSD){
 // console.log(convertUSDtoCAD(USD))
 
 console.log(`${USD} USD is equivalent to ${convertUSDtoCAD(USD)} CAD`)
+
+const tips = [5, 8, 13, 2, 14, 27, 9, 18]
+
+
+function calcTips (arr){
+    let sum = 0
+    for (let i=0; i < arr.length; i++ ){
+        sum = sum + arr[i]
+    }
+    return sum
+}
+
+console.log(calcTips(tips))
